2b7769886d90c1dc796afe684641e69ea659c0b1
Anthonin <anthonin.guillaume11@gmail.com>
2024-12-16 11:18:11 +0100

    mainQ1.c

--- mainQ1.c
+++ mainQ1.c
@@ -23,10 +23,48 @@
 
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
-int main()
-{
-	
+#define LINE "$ "
+#define GET "gettftp host file"
+#define PUT "puttftp host file"
+#define BSIZE 1024
+
+void line(){
+	write(STDOUT_FILENO, LINE, strlen(LINE));
+}
+
+void request(char *input, char *command, char *host, char *file){
+    
+    char buffer[BSIZE];
+    // The user command
+	ssize_t usercommand = read(STDIN_FILENO, buffer, sizeof(buffer) - 1);
+    // Remove newline and null-terminate
+    buffer[usercommand - 1] = '\0';	
+    
+    // Split input into 3 parts: command, host and file
+	if (sscanf(buffer, "%s %s %s", command, host, file) <3){
+		fprintf(stderr,"Invalid input (<command>tftp <host> <file>)\n");
+	// Check the command
+	} else if (strcmp(command, "gettftp") != 0 && strcmp(command, "puttftp") != 0) {
+		fprintf(stderr, "Unknown command (gettftp or puttftp)\n");
+	}
+}
+
+int main(){
+	while(1){
+		
+		line();
+		
+		char input[BSIZE];
+		char command[BSIZE];
+		char host[BSIZE];
+		char file[BSIZE];
+		request(input, command, host, file); // QUESTION 1
+				
+	}
 	return 0;
 }
 
